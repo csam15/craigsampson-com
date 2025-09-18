de8817256d82066537c77385c6923e5f9fc0cc3b
Craig Sampson <craigsjr15@gmail.com>
2025-09-17 22:46:58 -0400

    logo images updated

--- app/components/Header/Logo.tsx
+++ app/components/Header/Logo.tsx
@@ -4,15 +4,18 @@ import Image from "next/image";
 import Link from "next/link";
 import { useState, useRef } from "react";
 
-import image1 from "../../../public/Logo-images/3-cropped.svg";
-import image2 from "../../../public/Logo-images/4-cropped.svg";
-import image3 from "../../../public/Logo-images/5-cropped.svg";
-import image4 from "../../../public/Logo-images/6-cropped.svg";
+import image1 from "../../../public/Logo-images/3.svg";
+import image2 from "../../../public/Logo-images/4.svg";
+import image3 from "../../../public/Logo-images/5.svg";
+import image4 from "../../../public/Logo-images/6.svg";
+import image5 from "../../../public/Logo-images/7.svg";
+import image6 from "../../../public/Logo-images/8.svg";
+import image7 from "../../../public/Logo-images/9.svg";
 
 export default function Logo() {
   const [currentImage, setCurrentImage] = useState(0);
   const intervalRef = useRef<NodeJS.Timeout | null>(null);
-  const images = [image1, image2, image3, image4];
+  const images = [image1, image2, image3, image4, image5, image6, image7];
 
   // Change image every 3 seconds
   const changeImage = () => {
