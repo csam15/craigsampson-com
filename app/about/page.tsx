965b190e1523e424b9c16a21dd4e36b3bddd3903
Craig Sampson <craigsjr15@gmail.com>
2025-10-15 19:07:17 -0400

    about page created

---
+++ app/about/page.tsx
@@ -0,0 +1,14 @@
+export default function About() {
+  return (
+    <div className="page-container">
+      <div className="space-y-6 lg:space-y-8">
+        <h1>Hello There. I&apos;m Craig Sampson</h1>
+        <p>
+          I am a full stack developer and software engineer passionate about
+          building intuitive, efficient web applications.
+        </p>
+      </div>
+      <div></div>
+    </div>
+  );
+}
