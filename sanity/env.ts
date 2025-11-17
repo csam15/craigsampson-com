fee4c9c2977d51f049c8b2e689a0696496b4a91b
Craig Sampson <craigsjr15@gmail.com>
2025-11-17 12:07:48 -0500

    removed key from env.ts

--- sanity/env.ts
+++ sanity/env.ts
@@ -2,10 +2,10 @@ export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-11-16'
 
 export const dataset =
-  process.env.NEXT_PUBLIC_SANITY_DATASET || 'production'
+  process.env.NEXT_PUBLIC_SANITY_DATASET
 
 export const projectId =
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'z5pclmp9'
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
   if (v === undefined) {
